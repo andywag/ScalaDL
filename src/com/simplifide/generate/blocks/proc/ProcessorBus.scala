31b0011cfb33eef9aee58e2d22a02c6ee9c2f8bf
Andy <andy@andy-simulation.(none)>
2011-08-18 08:47:48 -0700

    Interim Checkin

---
+++ src/com/simplifide/generate/blocks/proc/ProcessorBus.scala
@@ -0,0 +1,34 @@
+package com.simplifide.generate.blocks.proc
+
+import com.simplifide.generate.blocks.basic.flop.ClockControl
+import com.simplifide.generate.signal.SignalTrait
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: andy
+ * Date: 8/18/11
+ * Time: 8:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ProcessorBus(val clk:ClockControl,
+  val writeAddress:SignalTrait,
+  val writeValid:SignalTrait,
+  val writeData:SignalTrait,
+  val readAddress:SignalTrait,
+  val readValid:SignalTrait,
+  val readData:SignalTrait) {
+
+
+
+}
+
+object ProcessorBus {
+  def apply(clk:ClockControl,
+            writeAddress:SignalTrait,
+            writeValid:SignalTrait,
+            writeData:SignalTrait,
+            readAddress:SignalTrait,
+            readValid:SignalTrait,
+            readData:SignalTrait) = new ProcessorBus(clk,writeAddress,writeValid,writeData,readAddress,readValid,readData)
+}
